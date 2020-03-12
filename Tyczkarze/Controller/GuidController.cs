a4ed253603f8df4640e19a4ad1d4a7ef59b6cf37
agatarychter <rychteragata@gmail.com>
2020-03-12 14:00:00 +0100

    Add registration controller with stubs

---
+++ Tyczkarze/Controller/GuidController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tyczkarze.BusinessLogic.Services;
+using Tyczkarze.BusinessLogic.Services.Interface;
+using Tyczkarze.DataAccess.Data;
+using Tyczkarze.DataAccess.Model;
+
+namespace Tyczkarze.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GuidController : ControllerBase
+    {
+        private readonly IAthleteService athleteService;
+        private readonly ApplicationDbContext _context;
+
+        public GuidController(ApplicationDbContext context)
+        {
+            athleteService = new AthleteService(context);
+            _context = context;
+        }
+
+        // GET: api/guid/id
+        [HttpPost("{id}")]
+        public ActionResult<Athlete> RemoveAthleteGuid(Int32 id)
+        {
+            var athlete = athleteService.RemoveAthleteGuid(id);
+            if (athlete == null)
+            {
+                return NotFound();
+            }
+            return athlete;
+        }
+
+
+        // POST: api/guid
+        [HttpPost]
+        public ActionResult<Athlete> VerifyAthleteByGuid(String guid)
+        {
+
+            var athleteGuid = athleteService.VerifyAthleteByGuid(guid);         
+
+            if(athleteGuid != null)
+            {
+                return Ok(athleteGuid);
+            }
+            else
+            {    
+                  return NotFound();
+                
+            }         
+            
+        }
+
+    }
+}
+
---
+++ Tyczkarze/Controller/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using Tyczkarze.BusinessLogic.Services;
+using Tyczkarze.BusinessLogic.Services.Interface;
+using Tyczkarze.DataAccess.Data;
+using Tyczkarze.DataAccess.Model;
+using Tyczkarze.DataAccess.Model.DTO;
+
+namespace Tyczkarze.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IAthleteService athleteService;
+        private readonly ApplicationDbContext _context;
+
+        public UserController(ApplicationDbContext context)
+        {
+            athleteService = new AthleteService(context);
+            _context = context;
+        }
+
+
+
+        // POST: api/user
+        [HttpPost]
+        public ActionResult<Athlete> VerifyUser(AthleteLoginDTO user)
+        {
+            var athlete = athleteService.VerifyAthlete(user);
+            if(athlete == null)
+                return NotFound();
+            return Ok(athlete);
+
+        }
+
+    }
+}
